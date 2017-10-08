ff04baf9de8db7a723e24c7a170ff192eda91e8c
Rick Li <ricklihs@hotmail.com>
2017-10-08 01:44:33 -0700

    update for branchWPF

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            txtBox1.Text = "BBCCDDD  FFFF";
+            txtBox1.Text = "BBCCDDD  FFFF For WPF";
         }
     }
 }
